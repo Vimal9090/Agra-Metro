74035f0296345cf0e559ade489a5bb907dc97709
vimal sharma <124519612+Vimal9090@users.noreply.github.com>
2023-08-05 15:08:10 +0530

    this is first upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    var x = document.getElementById("mobile");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+    //   x.style.display = "none";
+    }
+  }
+function myFunction1() {
+    var x = document.getElementById("mobile");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
